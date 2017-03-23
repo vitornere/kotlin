0a1d5831684aae511a19d037fd8e5bbd3d3ac3f0
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-23 14:08:30 +0300

    Revert "Advance idea.plugins.compatible.build"
    This reverts commit b101550cae36e98dad11957891ca1224654d483f.

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -338,7 +338,7 @@ class KotlinCoreEnvironment private constructor(
     }
 
     companion object {
-        private val ideaCompatibleBuildNumber = "172.9999"
+        private val ideaCompatibleBuildNumber = "162.9999"
 
         init {
             setCompatibleBuild()
