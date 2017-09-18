02cc5d0588d5ad2a10c34d6a6d404c09c1a670ef
Valentin Kipyatkov <valentin@jetbrains.com>
2017-09-18 18:43:30 +0300

    Support for optional parameters

--- idea/testData/replacementFor/stdlib/CreateTempFile.kt
+++ idea/testData/replacementFor/stdlib/CreateTempFile.kt
@@ -3,9 +3,8 @@ import java.io.File
 
 fun foo(dir: File) {
     val file1 = File.createTempFile("prefix", "suffix", dir)
-    //TODO:
-    //val file2 = File.createTempFile("prefix", "suffix", null)
-    //val file3 = File.createTempFile("prefix", "suffix")
-    //val file4 = File.createTempFile("prefix", null)
-    //val file5 = File.createTempFile("tmp", null)
+    val file2 = File.createTempFile("prefix", "suffix", null)
+    val file3 = File.createTempFile("prefix", "suffix")
+    val file4 = File.createTempFile("prefix", null) //TODO: no last argument
+    val file5 = File.createTempFile("tmp", null) //TODO: no arguments
 }
